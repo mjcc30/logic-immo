174340b7234803565b84180e7313ccd58d062e6d
mjcc30 <maxime.cordeiro@gmail.com>
2021-03-19 11:00:02 +0100

    add index.js

---
+++ front/index.js
@@ -0,0 +1,47 @@
+
+const express = require("express");
+const fetch = require("node-fetch");
+const path = require("path");
+
+const app = express();
+const URL_SERVER = "http://localhost:3000/";
+const PORT = "4000";
+const hostname = "localhost";
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res, next) => {
+  res.render("home");
+});
+
+app.get('/annonce', (req, res) => {
+  fetch(URL_SERVER, {
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((data) => {
+      return data.json();
+    })
+    .then((json) => {
+      res.render("annonce", { annonce: json });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/annonce/:id', (req, res) => {
+  fetch(`${URL_SERVER}/${req.params.id}`)
+    .then((data) => {
+      return data.json();
+    })
+    .then((json) => {
+      res.render("annonce", { annonce: json });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/create', (req, res) => {
+  res.render('addAnnonce');
+})
+
+app.listen(PORT, hostname, () => {
+  console.log(`Front is running at http://${hostname}:${PORT}/`);
+});
